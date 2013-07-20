cdf87caa494965672572a8ef5b8800f0b4b38b71
Jack <jack.wgm@gmail.com>
2013-07-20 16:42:53 +0800

    add header and examples.
    Signed-off-by: Jack <jack.wgm@gmail.com>

---
+++ include/websocket.hpp
@@ -0,0 +1,15 @@
+//
+// websocket.hpp
+// ~~~~~~~~~~~~~
+//
+// Copyright (c) 2013 Jack (jack at gmail dot com)
+//
+// Distributed under the GNU Affero General Public License, Version 3.0. (See accompanying
+// file LICENSE or copy at http://www.gnu.org/licenses/agpl-3.0.txt)
+//
+
+#ifndef AVWS_WEBSOCKET_HPP
+#define AVWS_WEBSOCKET_HPP
+
+
+#endif // AVWS_WEBSOCKET_HPP
